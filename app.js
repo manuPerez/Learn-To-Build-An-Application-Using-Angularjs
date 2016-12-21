dc29bb01d0347057bce54f77689383e09ba59550
Manu <perezdelgadomanu@gmail.com>
2016-12-21 21:22:40 +0100

    Custom directives

--- app.js
+++ app.js
@@ -64,25 +64,37 @@
 		this.products = gems;
 	});
 
-	app.controller('PanelController', function(){
-		this.tab = 1;
-
-		this.selectTab = function(secTab){
-			this.tab = secTab;
-		};
-		this.isSelected = function(checkTab){
-			return this.tab === checkTab;
-		}
-	});
-
 	app.controller('ReviewController', function(){
 		this.review = {};
 
 		this.addReview = function(product){
-			alert(this.review);
 			product.reviews.push(this.review);
 			this.review = {};
 		};
 	});
 	
+	app.directive('productTitle', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-title.html'
+		};
+	});
+
+	app.directive('productPanels', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function() {
+				this.tab = 1;
+
+				this.selectTab = function(secTab){
+					this.tab = secTab;
+				};
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				}
+			},
+			controllerAs: 'panels'
+		};
+	});
 })();
